53e23681ab5141379dd590f14723a4b66c6e33f7
Matt Zilley <matthew.zilley@gmail.com>
2026-02-17 23:24:39 -0600

    update total count in header.php

--- header.php
+++ header.php
@@ -9,34 +9,8 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 
-<?php if ( is_front_page() && function_exists( 'ncaa_render_sticky_nav' ) ) : ?>
+<?php if ( function_exists( 'ncaa_render_sticky_nav' ) ) : ?>
     <?php ncaa_render_sticky_nav(); ?>
-<?php else : ?>
-    <header id="site-header" class="site-header">
-        <div class="header-container">
-            <div class="site-branding">
-                <?php
-                if ( has_custom_logo() ) {
-                    the_custom_logo();
-                }
-                ?>
-            </div>
-
-            <?php
-            if ( has_nav_menu( 'primary' ) ) :
-                ?>
-                <nav class="main-navigation">
-                    <?php
-                    wp_nav_menu( array(
-                        'theme_location' => 'primary',
-                        'menu_class'     => 'primary-menu',
-                        'container'      => false,
-                    ) );
-                    ?>
-                </nav>
-            <?php endif; ?>
-        </div>
-    </header>
 <?php endif; ?>
 
 <main id="main-content" class="site-main">
